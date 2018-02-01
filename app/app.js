b161324e99afa1d2a92108c843a14429c34a8223
Aaron Wolfcale <aaronwolfcale@Aarons-MacBook-Pro.local>
2018-02-01 11:21:36 -0600

    adding route-listing

--- app/app.js
+++ app/app.js
@@ -3,13 +3,16 @@
 angular.module("route", ["ngRoute"])
 .config($routeProvider => {
   $routeProvider
+    .when("/", {
+        templateUrl: "partials/listing.html"
+    })
     .when("/blueridgeparkway", {
       templateUrl: "partials/blue-ridge.html",
       controller: "BlueRidge"
     })
     .when("/route66", {
       templateUrl: "partials/route66.html",
-      controller: "Route66",
+      controller: "Route66"
     })
     .otherwise("/");
 
